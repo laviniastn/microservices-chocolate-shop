aed495bab34dbdaba864b48ae08e9c698af2a4ad
laviniastn <laviniastn25@gmail.com>
2023-03-05 20:36:48 +0200

    init

---
+++ chocolateshop/src/main/java/com/choco/chocolateshop/ChocolateshopApplication.java
@@ -0,0 +1,13 @@
+package com.choco.chocolateshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocolateshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocolateshopApplication.class, args);
+	}
+
+}
---
+++ chocolateshop/src/test/java/com/choco/chocolateshop/ChocolateshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choco.chocolateshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocolateshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
